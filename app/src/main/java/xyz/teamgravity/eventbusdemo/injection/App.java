8604b4e56eebd14907267bfc09c6de068c1a8ebc
raheemadamboev <vividricemaryandmax@gmail.com>
2022-06-14 16:18:31 +0500

    install event bus index

--- app/src/main/java/xyz/teamgravity/eventbusdemo/injection/App.java
+++ app/src/main/java/xyz/teamgravity/eventbusdemo/injection/App.java
@@ -2,11 +2,13 @@ package xyz.teamgravity.eventbusdemo.injection;
 
 import android.app.Application;
 
+import org.greenrobot.eventbus.EventBus;
+
 public class App extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-
+        EventBus.builder().addIndex(new EventBusIndex()).installDefaultEventBus();
     }
 }
