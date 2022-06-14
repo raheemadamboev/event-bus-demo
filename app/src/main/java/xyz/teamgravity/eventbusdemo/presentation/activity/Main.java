3b7c3d06b721234a69ed7f6dc7272458b7f88422
raheemadamboev <vividricemaryandmax@gmail.com>
2022-06-14 14:53:32 +0500

    setup  binding

--- app/src/main/java/xyz/teamgravity/eventbusdemo/presentation/activity/Main.java
+++ app/src/main/java/xyz/teamgravity/eventbusdemo/presentation/activity/Main.java
@@ -1,16 +1,19 @@
 package xyz.teamgravity.eventbusdemo.presentation.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-import xyz.teamgravity.eventbusdemo.R;
+import androidx.appcompat.app.AppCompatActivity;
+
+import xyz.teamgravity.eventbusdemo.databinding.ActivityMainBinding;
 
 public class Main extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
