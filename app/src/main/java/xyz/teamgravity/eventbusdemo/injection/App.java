d13d44d5348e823302b15ef941e8a4330b4371cb
raheemadamboev <vividricemaryandmax@gmail.com>
2022-06-14 15:04:46 +0500

    setup injection

---
+++ app/src/main/java/xyz/teamgravity/eventbusdemo/injection/App.java
@@ -0,0 +1,12 @@
+package xyz.teamgravity.eventbusdemo.injection;
+
+import android.app.Application;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
