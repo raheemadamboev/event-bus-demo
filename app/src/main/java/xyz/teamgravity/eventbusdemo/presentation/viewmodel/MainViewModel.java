beef47f150f697291fbebf01bd331b6244775694
raheemadamboev <vividricemaryandmax@gmail.com>
2022-06-14 15:11:05 +0500

    generate random number post event

--- app/src/main/java/xyz/teamgravity/eventbusdemo/presentation/viewmodel/MainViewModel.java
+++ app/src/main/java/xyz/teamgravity/eventbusdemo/presentation/viewmodel/MainViewModel.java
@@ -2,9 +2,19 @@ package xyz.teamgravity.eventbusdemo.presentation.viewmodel;
 
 import androidx.lifecycle.ViewModel;
 
+import org.greenrobot.eventbus.EventBus;
+
+import java.util.Random;
+
 public class MainViewModel extends ViewModel {
 
-    public void onGenerate() {
+    private final Random random;
 
+    public MainViewModel() {
+        random = new Random();
+    }
+
+    public void onGenerate() {
+        EventBus.getDefault().post(new MainEvent(String.valueOf(random.nextInt(999999))));
     }
 }
